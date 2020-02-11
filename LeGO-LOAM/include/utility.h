e46e323a11c0db50d60c76d084f6fda7d2bd291e
Engin Bozkurt <engin.bozkurt034@gmail.com>
2020-02-12 00:56:32 +0300

    Update utility.h

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -50,7 +50,7 @@ using namespace std;
 
 typedef pcl::PointXYZI  PointType;
 
-extern const string pointCloudTopic = "/velodyne_points";
+extern const string pointCloudTopic = "/Sensor/points";
 extern const string imuTopic = "/imu/data";
 
 // Save pcd
